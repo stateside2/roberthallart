1c63d8f18bb4839304e65986c7b99c1187b5d848
stateside2 <ianedwardgrant@gmail.com>
2026-03-04 14:27:47 +0000

    009 Added About Me/Commissions text

--- js/intro.js
+++
@@ -1,7 +0,0 @@
-
-// Wait 3.5 seconds, then slide intro up
-setTimeout(() => {
-    const intro = document.getElementById("intro-container");
-    intro.classList.add("slide-up");
-}, 3500);   // delay before sliding (you can change this)
-
